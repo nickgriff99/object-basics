3d84020b02efcb95fb86ddf67b3ceeebeaf95fb0
Nicholas Griffiths <nichg999@gmail.com>
2025-01-27 17:43:55 -0700

    finished all tasks for first exercise-1

--- exercise-1.js
+++ exercise-1.js
@@ -4,3 +4,34 @@ Start with exercise-1.md if you haven't read it!
 */
 
 // YOUR WORK GOES HERE //
+
+const tucanos = {
+  name: 'Tucanos',
+  cuisines: ['Brazilian', 'Steakhouse'],
+  numberOfStars: 4.5,
+  favorited: true,
+};
+
+tucanos.address = '555 Main St';
+tucanos.zipcode = 84101;
+tucanos.acceptsReservations = true;
+tucanos.numberOfStars = 5;
+tucanos.favorited = false;
+tucanos.cuisines.push('Seafood');
+
+function retrieveProperty(obj, key) {
+  if (obj.hasOwnProperty(key)) {
+    console.log(obj[key]);
+  }
+    else {
+    console.log('The information you requested does not exist.');
+  }
+}
+
+retrieveProperty(tucanos, 'cuisines');
+retrieveProperty(tucanos, 'favorited');
+retrieveProperty(tucanos, 'name');
+retrieveProperty(tucanos, 'nickName');
+retrieveProperty(tucanos, 'state');
+
+console.log(tucanos);
